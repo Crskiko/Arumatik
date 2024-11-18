d20dd3b85eb3fa26bf6bb7665ae2a0810efa7f96
syahba <ksyaahba@gmail.com>
2024-11-18 11:07:43 +0700

    add dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: '/src',
-      components: '/src/components',
-      assets: '/src/assets',
-      hooks: '/src/hooks',
-      utils: '/src/utils',
-    }
-  }
+  plugins: [react()]
 })
