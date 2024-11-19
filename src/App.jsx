978aa693aa04566ad64ae4cf9f94e50d0c6ba327
syahba <ksyaahba@gmail.com>
2024-11-19 20:42:15 +0700

    fox bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
 import ProductListPage from "./pages/ProductListPage"
 import ProductDetailPage from "./pages/ProductDetailPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <BrowserRouter future={{ v7_relativeSplatPath: true }}>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/products" element={<ProductListPage></ProductListPage>}></Route>
         <Route path="/products/:id" element={<ProductDetailPage></ProductDetailPage>}></Route>
       </Routes>
