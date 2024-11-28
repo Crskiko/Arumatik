0e29f4509ef4cbacc981dd2cd7899c27272d973c
syahba <ksyaahba@gmail.com>
2024-11-28 23:26:48 +0700

    add jsdoc to app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import ProductList from "./pages/ProductList"
-import ProductDetail from "./pages/ProductDetail"
-import Home from "./pages/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import Home from "./pages/Home";
 
+/**
+ * Main App component that defines the application's routing structure.
+ * 
+ * It uses React Router to define routes for the home page, product list, 
+ * and individual product details. The routes are rendered within a 
+ * `BrowserRouter` component.
+ * 
+ * @returns {JSX.Element} The rendered JSX of the main application with routing.
+ */
 function App() {
   return (
     <BrowserRouter future={{ v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/products" element={<ProductList></ProductList>}></Route>
-        <Route path="/products/:productName" element={<ProductDetail></ProductDetail>}></Route>
+        <Route
+          path="/products/:productName"
+          element={<ProductDetail></ProductDetail>}
+        ></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { ProductProvider } from "./contexts/ProductContext.jsx";
 
+/**
+ * Entry point for the React application.
+ * 
+ * This file renders the `App` component inside the `StrictMode` wrapper and 
+ * wraps it in the `ProductProvider` context provider to manage product-related state.
+ * 
+ * @returns {void} This function does not return anything as it renders the app to the DOM.
+ */
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ProductProvider>
